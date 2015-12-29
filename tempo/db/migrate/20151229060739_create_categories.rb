c91697dcd9b1f829612dca99be94da83ccd72541
r.sinclairchin <r.sinclairchin@gmail.com>
2015-12-28 22:08:26 -0800

    add project and category models

---
+++ tempo/db/migrate/20151229060739_create_categories.rb
@@ -0,0 +1,11 @@
+class CreateCategories < ActiveRecord::Migration
+  def change
+    create_table :categories do |t|
+      t.integer :timebox
+      t.string :name
+      t.integer :time
+
+      t.timestamps null: false
+    end
+  end
+end
