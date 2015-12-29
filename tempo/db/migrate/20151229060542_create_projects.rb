c75aec5e9e08d0a1541b6a8857439dbedf5bb7d2
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2015-12-28 22:28:17 -0800

    Create associations for tables

--- tempo/db/migrate/20151229060542_create_projects.rb
+++ tempo/db/migrate/20151229060542_create_projects.rb
@@ -2,6 +2,7 @@ class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
       t.integer :time
+      t.integer :timebox
       t.string :name
       t.string :description
 
