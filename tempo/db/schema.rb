a01275280ff1d21931a07ce7ba3dfed39d7f00ea
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2015-12-28 22:44:18 -0800

    Edit user schema

--- tempo/db/schema.rb
+++ tempo/db/schema.rb
@@ -11,10 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151228220129) do
+ActiveRecord::Schema.define(version: 20151229060739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.integer  "timebox"
+    t.string   "name"
+    t.integer  "time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.integer  "time"
+    t.integer  "timebox"
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
